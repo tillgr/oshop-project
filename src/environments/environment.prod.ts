b0eb60b7bc801f637cc3fa554188b6001f8eb646
Till <till2000@live.com>
2021-11-23 20:45:04 +0100

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAGCoDBtGzDaWbTVxzwVdkQ99FFAf17lrc',
+    authDomain: 'oshop-project-980ad.firebaseapp.com',
+    databaseURL: 'https://oshop-project-980ad.firebaseio.com',
+    projectId: 'oshop-project-980ad',
+    storageBucket: 'oshop-project-980ad.appspot.com',
+    messagingSenderId: '911499671741',
+    appId: '1:911499671741:web:c1cbfeb448e42dafa79fd4',
+    measurementId: 'G-5389QJ3EVT',
+  }
 };
