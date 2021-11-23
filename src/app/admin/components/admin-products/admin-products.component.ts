c93f0738e9fb937e9e8790fdd320ae5f61f79d1d
Till <till2000@live.com>
2021-11-23 21:16:47 +0100

    format

--- src/app/admin/components/admin-products/admin-products.component.ts
+++ src/app/admin/components/admin-products/admin-products.component.ts
@@ -1,8 +1,8 @@
-import { Product } from '../../../shared/models/product';
-import { Subscription } from 'rxjs/Subscription';
-import { ProductService } from '../../../shared/services/product.service';
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { DataTableResource } from 'angular-4-data-table';
+import {Product} from '../../../shared/models/product';
+import {Subscription} from 'rxjs/Subscription';
+import {ProductService} from '../../../shared/services/product.service';
+import {Component, OnInit, OnDestroy} from '@angular/core';
+import {DataTableResource} from 'angular-4-data-table';
 
 @Component({
   selector: 'app-admin-products',
@@ -14,9 +14,9 @@ export class AdminProductsComponent implements OnInit, OnDestroy {
   subscription: Subscription;
   tableResource: DataTableResource<Product>;
   items: Product[] = [];
-  itemCount: number; 
+  itemCount: number;
 
-  constructor(private productService: ProductService) { 
+  constructor(private productService: ProductService) {
     this.subscription = this.productService.getAll()
       .subscribe(products => {
         this.products = products;
@@ -26,20 +26,22 @@ export class AdminProductsComponent implements OnInit, OnDestroy {
 
   private initializeTable(products: Product[]) {
     this.tableResource = new DataTableResource(products);
-    this.tableResource.query({ offset: 0 })
+    this.tableResource.query({offset: 0})
       .then(items => this.items = items);
     this.tableResource.count()
       .then(count => this.itemCount = count);
   }
 
   reloadItems(params) {
-    if (!this.tableResource) return;
+    if (!this.tableResource) {
+      return;
+    }
 
     this.tableResource.query(params)
-      .then(items => this.items = items);    
+      .then(items => this.items = items);
   }
 
-  filter(query: string) { 
+  filter(query: string) {
     let filteredProducts = (query) ?
       this.products.filter(p => p.title.toLowerCase().includes(query.toLowerCase())) :
       this.products;
